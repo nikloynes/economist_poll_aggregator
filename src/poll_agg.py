8bf1e119e185f340e4e5900821b41278ead8284b
nikloynes <niklasloynes@gmail.com>
2023-08-28 18:21:49 +0100

    made more progress on aggregating function with a lot of options.

--- src/poll_agg.py
+++ src/poll_agg.py
@@ -60,6 +60,9 @@ TRENDS_OUTFILE = ut.load_env_var('TRENDS_OUTFILE')
 # columns in every polls_df
 BASE_COLS = ['date', 'pollster', 'n']
 
+# columns to drop in trends_df
+TRENDS_DROP = ['pollster', 'n']
+
 ###########
 # FUNCTIONS
 ###########
@@ -130,8 +133,9 @@ def aggregate_polls(polls_df: pd.DataFrame,
                     candidates: list[str] | str = 'all',
                     agg_type: Literal['mean', 'median'] = 'mean',
                     increment_days: int = 1,
-                    lead_time: int = 0,
-                    lead_mechanism: Literal['if_missing', 'always', 'never'] = 'if_missing',
+                    min_lead_time: int = 0,
+                    max_lead_time: int = 7,
+                    interpolation: Literal['if_missing', 'always', 'never'] = 'if_missing',
                     from_date: dt.datetime | str = None,
                     to_date: dt.datetime | str = None) -> pd.DataFrame:
     '''
@@ -150,9 +154,9 @@ def aggregate_polls(polls_df: pd.DataFrame,
         :agg_type (str): type of aggregation to use, either 'mean' or 'median'
         :increment_days (int): number of days to increment by when aggregating polls, 
             defaults to 1 (daily)
-        :lead_time (int): number of days to include in an average
+        :lead_time (int): number of days before target day to include in an average
             when aggregating polls
-        :lead_mechanism (str): mechanism to use when leading polls, 
+        :interpolation (str): mechanism to use when leading polls, 
             either 'if_missing', 'always' or 'never'. 'if_missing' 
             will include data from `lead_time` previous days only if 
             there is no data for a given day. `always` will always include
@@ -193,10 +197,13 @@ def aggregate_polls(polls_df: pd.DataFrame,
     if not isinstance(increment_days, int):
         raise TypeError('increment_days must be an int')
     
-    if not isinstance(lead_time, int):
-        raise TypeError('lead_time must be an int')
+    if not isinstance(min_lead_time, int):
+        raise TypeError('min_lead_time must be an int')
     
-    if lead_mechanism not in ['if_missing', 'always', 'never']:
+    if not isinstance(max_lead_time, int):
+        raise TypeError('max_lead_time must be an int')
+    
+    if interpolation not in ['if_missing', 'always', 'never']:
         raise ValueError('lead_mechanism must be either "if_missing", "always" or "never"')
     
     # filter polls_df by date if required
@@ -212,13 +219,62 @@ def aggregate_polls(polls_df: pd.DataFrame,
         polls_df = polls_df[polls_df['date'] <= to_date]
         logging.info(f'filtered polls by to_date: {to_date}')
 
-    # drop 'pollster' col
-    polls_df = polls_df.drop(columns=['pollster'])
+    # remove redundant columns
+    polls_df = polls_df.drop(columns=TRENDS_DROP)
 
-    # aggregate
-    if lead_time==0:
-        trends_df = polls_df.groupby('date').agg(agg_type)
-    else:
-        raise NotImplementedError('lead_time > 0 not yet implemented')
+    # log some info about the supplied data, our aggregation mechanism,
+    # and also instantiate some vars
+    logging.info(f'n polls retrieved: {len(polls_df)}')
 
-    return trends_df
+    start_date = polls_df['date'].min()
+    end_date = polls_df['date'].max()
+    logging.info(f'earliest date with polls: {start_date}')
+    logging.info(f'latest date with polls: {end_date}')
+    
+    # create a list of all dates between start and end date
+    # with increments of increment_days
+    dates = [start_date + dt.timedelta(days=x) for x in range(0, (end_date-start_date).days, increment_days)] 
+
+    logging.info(f'polling averages produced in increments of {increment_days} days')
+
+    logging.info(f'interpolation for days with missing data: {interpolation}')
+    logging.info(f'lead time: {min_lead_time} days to {max_lead_time} days. this is days to include in aggregation per increment.')
+
+    logging.info(f'aggregation method: {agg_type}.')
+
+    logging.info(f'aggregating data for candidates: {candidates}')
+
+    # start by aggregating data
+    trends_df = polls_df.groupby('date').agg(agg_type).reset_index()
+    
+    if interpolation=='never':
+        logging.info(f'not interpolating data. finished aggregating polls.')
+        out_df = trends_df
+
+    elif interpolation=='if_missing':
+        logging.info(f'interpolating data for dates with missing poll data...')
+        out_df = pd.DataFrame()
+        for date in dates:
+            if date in trends_df['date']:
+                # copy data from trends_df to out_df
+                logging.info(f'found data for {date}. retaining.')
+                out_df = out_df.append(trends_df[trends_df['date']==date])
+            else:
+                logging.info(f'no data for {date}. interpolating...')
+                # QUESTION HERE IS IF WE WANT TO CHECK IN POLLS DF OR IN TRENDS. 
+                # IT DOESNT REALLY MATTER BUT WE MIGHT BE slightly more granula if checking
+                # polls.
+                subset_df = polls_df[polls_df['date']>=date-dt.timedelta(days=max_lead_time)]
+                subset_df = subset_df[subset_df['date']<=date-dt.timedelta(days=min_lead_time)]
+                # produce means for each column that isn't `date`
+                subset_df = subset_df.groupby('date').agg(agg_type).reset_index()
+
+    elif interpolation=='always':
+        # iterate over `dates`.
+        # if data exist in trends_df for that date, copy to out_df
+        # if data do not exist in trends_df, combine days from 
+        for date in dates:
+            pass
+
+
+    return out_df
